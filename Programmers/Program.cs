5b701b4a1558827e0a5a67546b57f60149ad5527
DESKTOP-9RB0GR6\tjrdud <hsy961961@gmail.com>
2022-06-27 23:18:21 +0900

    없는 숫자 더하기 문제

--- Programmers/Program.cs
+++ Programmers/Program.cs
@@ -8,7 +8,19 @@ namespace Programmers
         {
             Console.WriteLine("Hello World!");
 
-            //asdfasdfasdfasdf
+
+        }
+
+        public int solution(int[] numbers)
+        {
+            int answer = 45;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer -= numbers[i];
+            }
+
+            return answer;
         }
     }
 }
